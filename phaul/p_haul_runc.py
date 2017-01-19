2150b6cc6ad39ec5b9ee38353de48256585195d9
Veronika Kabatova <vkabatov@redhat.com>
2017-01-19 16:58:29 +0100

    Match code style to `Make lint`

--- phaul/p_haul_runc.py
+++ phaul/p_haul_runc.py
@@ -27,11 +27,10 @@ class p_haul_type(object):
 
 		# Validate provided container ID
 		if (not(re.match("^[\w-]+$", ctid)) or len(ctid) > 1024):
-			raise Exception("Invalid runc container name: %s",
-					ctid)
+			raise Exception("Invalid runc container name: %s", ctid)
 
 		self._ctid = ctid
-		self._veths = []	# TODO
+		self._veths = []  # TODO
 		self._binds = {}
 		self._inherit_fd = {}
 
@@ -42,8 +41,7 @@ class p_haul_type(object):
 			for line in proc_cgroups.readlines():
 				parts = line.split(":")
 				if len(parts) < 3:
-					logging.error("Invalid cgroup: %s",
-							line)
+					logging.error("Invalid cgroup: %s", line)
 				else:
 					subsystems = parts[1].split(",")
 					for subsystem in subsystems:
@@ -57,20 +55,14 @@ class p_haul_type(object):
 		try:
 			with open(runc_run + self._ctid + "/state.json",
 					"r") as state:
-				self._container_state = json.loads(
-					state.read())
-			self._labels = self._container_state[
-						"config"]["labels"]
-			self._ct_rootfs = self._container_state[
-						"config"]["rootfs"]
-			self._root_pid = self._container_state[
-						"init_process_pid"]
+				self._container_state = json.loads(state.read())
+			self._labels = self._container_state["config"]["labels"]
+			self._ct_rootfs = self._container_state["config"]["rootfs"]
+			self._root_pid = self._container_state["init_process_pid"]
 			self._ext_descriptors = json.dumps(
-					self._container_state[
-						"external_descriptors"])
+				self._container_state["external_descriptors"])
 		except IOError:
-			raise Exception("No container %s is running",
-					self._ctid)
+			raise Exception("No container %s is running", self._ctid)
 		except KeyError:
 			raise Exception("Invalid container state retrieved")
 
@@ -83,10 +75,8 @@ class p_haul_type(object):
 			cgroup_paths = self._container_state["cgroup_paths"]
 		for mount in self._container_state["config"]["mounts"]:
 			if mount["device"] == "bind":
-				if mount["destination"].startswith(
-						self._ct_rootfs):
-					dst = mount["destination"][len(
-							self._ct_rootfs):]
+				if mount["destination"].startswith(self._ct_rootfs):
+					dst = mount["destination"][len(self._ct_rootfs):]
 				else:
 					dst = mount["destination"]
 				self._binds.update({dst: dst})
@@ -94,20 +84,16 @@ class p_haul_type(object):
 				for subsystem, c_mp in cgroup_paths.items():
 					# Remove container ID from path
 					mountpoint = os.path.split(c_mp)[0]
-					dst = os.path.join(
-							mount["destination"],
-							os.path.split(
-								mountpoint)[0])
+					dst = os.path.join(mount["destination"],
+							os.path.split(mountpoint)[0])
 					if dst.startswith(self._ct_rootfs):
-						dst = dst[len(
-							self._ct_rootfs):]
+						dst = dst[len(self._ct_rootfs):]
 					self._binds.update({dst: dst})
 
 		if self._container_state["config"]["mask_paths"] is not None:
 			masked = self._container_state["config"]["mask_paths"]
 			for path in masked:
-				filepath = os.path.join("/proc",
-						self.root_task_pid,
+				filepath = os.path.join("/proc", self.root_task_pid,
 						"root", path)
 				if (os.path.exists(filepath) and
 						not os.path.isdir(filepath)):
@@ -169,8 +155,7 @@ class p_haul_type(object):
 		pass
 
 	def get_fs(self, fdfs=None):
-		return fs_haul_subtree.p_haul_fs([self._ct_rootfs,
-							self._ct_config])
+		return fs_haul_subtree.p_haul_fs([self._ct_rootfs, self._ct_config])
 
 	def get_fs_receiver(self, fdfs=None):
 		return None
@@ -182,12 +167,10 @@ class p_haul_type(object):
 		desc_path = os.path.join(path, "descriptors.json")
 		with open(desc_path, "w+") as desc_file:
 			desc_file.write(self._ext_descriptors)
-		shutil.copy(os.path.join(runc_run, self._ctid, "state.json"),
-				path)
+		shutil.copy(os.path.join(runc_run, self._ctid, "state.json"), path)
 		return [(bundle_path, "bundle.txt"),
 				(desc_path, "descriptors.json"),
-				(os.path.join(path, "state.json"),
-						"state.json")]
+				(os.path.join(path, "state.json"), "state.json")]
 
 	def put_meta_images(self, dir):
 		with open(os.path.join(dir, "bundle.txt")) as bundle_file:
@@ -207,10 +190,8 @@ class p_haul_type(object):
 
 	def final_restore(self, img, connection):
 		try:
-			with open(self._runc_bundle + "/config.json",
-					"r") as config:
-				self._container_state = json.loads(
-							config.read())
+			with open(self._runc_bundle + "/config.json", "r") as config:
+				self._container_state = json.loads(config.read())
 			root_path = self._container_state["root"]["path"]
 		except IOError:
 			raise Exception("Unable to get container config")
@@ -218,8 +199,7 @@ class p_haul_type(object):
 			raise Exception("Invalid config")
 
 		if not os.path.isabs(root_path):
-			self._ct_rootfs = os.path.join(self._runc_bundle,
-							root_path)
+			self._ct_rootfs = os.path.join(self._runc_bundle, root_path)
 		else:
 			self._ct_rootfs = root_path
 
@@ -229,38 +209,30 @@ class p_haul_type(object):
 			cgroup_paths = self._container_state["cgroup_paths"]
 		for mount in self._container_state["mounts"]:
 			if mount["type"] == "bind":
-				if mount["destination"].startswith(
-							self._ct_rootfs):
-					dst = mount["destination"][len(
-							self._ct_rootfs):]
+				if mount["destination"].startswith(self._ct_rootfs):
+					dst = mount["destination"][len(self._ct_rootfs):]
 				else:
 					dst = mount["destination"]
 				self._binds.update({dst: mount["source"]})
 			if mount["type"] == "cgroup":
-				with open("/proc/self/mountinfo",
-							"r") as mountinfo:
+				with open("/proc/self/mountinfo", "r") as mountinfo:
 					lines = mountinfo.readlines()
 				for subsystem, c_mp in cgroup_paths.items():
 					# Remove container ID from path
 					mountpoint = os.path.split(c_mp)[0]
-					dst = os.path.join(
-							mount["destination"],
-							os.path.split(
-								mountpoint)[0])
+					dst = os.path.join(mount["destination"],
+							os.path.split(mountpoint)[0])
 					if dst.startswith(self._ct_rootfs):
-						dst = dst[len(
-							self._ct_rootfs):]
+						dst = dst[len(self._ct_rootfs):]
 					line = next(line for line in lines
 							if mountpoint in line)
 					src = os.path.join(mountpoint,
 						os.path.relpath(
-							self_cgroups[
-								subsystem],
+							self_cgroups[subsystem],
 							line.split()[3]))
 					self._binds.update({dst: src})
 
-		with open(os.path.join(img.image_dir(), "descriptors.json"),
-				"r") as descr:
+		with open(os.path.join(img.image_dir(), "descriptors.json"), "r") as descr:
 			inherits = [(dsc, i) for i, dsc in
 					enumerate(json.loads(descr.read()))
 					if "pipe:" in dsc]
@@ -270,8 +242,7 @@ class p_haul_type(object):
 		ct_path = os.path.join(runc_run, self._ctid)
 		if not os.path.exists(ct_path):
 			os.makedirs(ct_path)
-		with open(os.path.join(img.image_dir(), "state.json"),
-				"r") as old_state_file:
+		with open(os.path.join(img.image_dir(), "state.json"), "r") as old_state_file:
 			self._restore_state = json.loads(old_state_file.read())
 
 		criu_cr.criu_restore(self, img, connection)
@@ -282,7 +253,6 @@ class p_haul_type(object):
 				"state.json"), "w+") as new_state_file:
 			new_state_file.write(json.dumps(self._restore_state))
 		self.umount()
-
 
 	def can_pre_dump(self):
 		return True
