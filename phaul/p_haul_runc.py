669b4c8b789fef5fc0ecf642cfbda6a80ce167f0
Veronika Kabatova <vkabatov@redhat.com>
2017-02-06 12:42:24 +0100

    Add action script function

--- phaul/p_haul_runc.py
+++ phaul/p_haul_runc.py
@@ -276,6 +276,9 @@ class p_haul_type(object):
 	def veths(self):
 		return self._veths
 
+	def run_action_scripts(self, stage):
+		pass
+
 	def net_lock(self):
 		for veth in self._veths:
 			util.ifdown(veth.pair)
